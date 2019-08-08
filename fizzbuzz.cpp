96f99c3136d576eb378b877b9c5cadb7e458a38c
TinghuiSong <43787998+TinghuiSong@users.noreply.github.com>
2019-08-08 01:20:46 +0100

    added bool functionality (#3)

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -2,22 +2,29 @@
 
 using namespace std;
 
-void fizzbuzz( int maxVal){
-	for (int i = 0; i <= maxVal; ++i){
-                if ( i%4 == 0 && i%6 == 0){
-                        cout << "fizzbuzz ";
-                }
-                else if ( i%4 == 0 ){
-                        cout << "fizz ";
-                }
-                else if ( i%6 == 0 ){
-                        cout << "buzz ";
-                }
-                else{
-                        cout << i << " ";
-                }
-        }
+void fizzbuzz (int maxVal){
+	bool fizzbool = true;
+	for (int i = 0; i < 100; ++i){
+		fizzbool = true;
+		if (i % 4 == 0){
+			cout << "fizz";
+			fizzbool = false;
+		}
+		if(i % 6 == 0){
+			cout << "buzz";
+			fizzbool = false;
+		}
+		if(i % 5 == 0){
+			cout << "bool";
+			fizzbool = false;
+		}
+		if(fizzbool){
+			cout<< i;
+		}
+		cout << endl;
+	}
 }
+
 int main(){
 	fizzbuzz(100)	;
   return 0;
