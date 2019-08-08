73bb6489ddec0a60faa9fa1f593b6a46796be4d7
TinghuiSong <43787998+TinghuiSong@users.noreply.github.com>
2019-08-08 01:05:56 +0100

    made fizzbuzz into a function (#2)

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -2,20 +2,23 @@
 
 using namespace std;
 
-int main(){
-	for (int i = 0; i < 101; ++i){
-		if ( i%4 == 0 && i%6 == 0){
-			cout << "fizzbuzz ";
-		}
-		else if ( i%4 == 0 ){
-			cout << "fizz ";
-		}
-		else if ( i%6 == 0 ){
-			cout << "buzz ";
-		}
-		else{
-			cout << i << " ";
-		}
-	}
-	return 0;
+void fizzbuzz( int maxVal){
+	for (int i = 0; i <= maxVal; ++i){
+                if ( i%4 == 0 && i%6 == 0){
+                        cout << "fizzbuzz ";
+                }
+                else if ( i%4 == 0 ){
+                        cout << "fizz ";
+                }
+                else if ( i%6 == 0 ){
+                        cout << "buzz ";
+                }
+                else{
+                        cout << i << " ";
+                }
+        }
 }
+int main(){
+	fizzbuzz(100)	;
+  return 0;
+  }
